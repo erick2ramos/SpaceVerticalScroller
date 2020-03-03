be123eb7ac20ef80cc923a5535ad85b959633c3f
Erick Dos Ramos <erick2ramos@gmail.com>
2020-03-03 01:07:23 -0300

    Simple binary data persistance manager

---
+++ VerticalScroller/Assets/01_Scripts/DataPersistance/DataPersistanceManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using BaseSystems.Managers;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BaseSystems.DataPersistance
+{
+    public class DataPersistanceManager : Manager
+    {
+        public static PlayerData PlayerData;
+
+        BinaryFormatter _formatter;
+        const string FILE_EXT = ".sdata";
+
+        public override void Initialize()
+        {
+            _formatter = new BinaryFormatter();
+            Load();
+        }
+
+        public void Save()
+        {
+            string filePath = Path.Combine(Application.persistentDataPath, "save" + FILE_EXT);
+            FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate);
+
+            _formatter.Serialize(fs, PlayerData);
+            Debug.LogWarning(JsonUtility.ToJson(PlayerData));
+            fs.Close();
+        }
+
+        public void Load()
+        {
+            string filePath = Path.Combine(Application.persistentDataPath, "save" + FILE_EXT);
+            if (!File.Exists(filePath))
+            {
+                PlayerData = new PlayerData();
+                return;
+            }
+            FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+            PlayerData = (PlayerData)_formatter.Deserialize(fs);
+            fs.Close();
+        }
+
+#if UNITY_EDITOR
+        [ContextMenu("Delete saved data")]
+        public void Delete()
+        {
+            string filePath = Path.Combine(Application.persistentDataPath, "save" + FILE_EXT);
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+#endif
+    }
+}
---
+++ VerticalScroller/Assets/01_Scripts/DataPersistance/PlayerData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BaseSystems.DataPersistance
+{
+    // Used to store player data to save in a binary file
+    [System.Serializable]
+    public class PlayerData
+    {
+        public int CurrentHighScore;
+        
+        public PlayerData()
+        {
+            
+        }
+    }
+}
