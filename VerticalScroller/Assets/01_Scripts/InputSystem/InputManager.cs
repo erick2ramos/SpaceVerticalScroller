965460a7602c6a7feada5a861980440f059fce46
Erick Dos Ramos <erick2ramos@gmail.com>
2020-03-03 01:01:36 -0300

    Simple key input manager

--- VerticalScroller/Assets/01_Scripts/InputSystem/InputManager.cs
+++ VerticalScroller/Assets/01_Scripts/InputSystem/InputManager.cs
@@ -6,9 +6,31 @@ namespace BaseSystems.InputSystem
 {
     public class InputManager : Manager
     {
+        Vector3 _mainMovement;
+        public Vector3 MovementDirection { get { return _mainMovement; } }
+        public bool FireButtonDown { get; private set; }
+
         public override void Initialize()
         {
             base.Initialize();
+        }
+
+        private void Update()
+        {
+            float horizontalMovement = Input.GetAxis("Horizontal");
+            float verticalMovement = Input.GetAxis("Vertical");
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                FireButtonDown = true;
+            }
+            if (Input.GetButtonUp("Fire1"))
+            {
+                FireButtonDown = false;
+            }
+
+            _mainMovement.x = horizontalMovement;
+            _mainMovement.z = verticalMovement;
         }
     }
 }
