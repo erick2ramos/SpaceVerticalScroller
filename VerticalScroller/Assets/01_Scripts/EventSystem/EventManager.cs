f1ff0ab1f3fc5a166c1dd569cabc0e7e68aee34d
Erick Dos Ramos <erick2ramos@gmail.com>
2020-03-03 23:10:35 -0300

    Adding event manager/listener

---
+++ VerticalScroller/Assets/01_Scripts/EventSystem/EventManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BaseSystems.EventSystem
+{
+    public interface IEventListenerBase { }
+    public interface IEventListener<EventType> : IEventListenerBase 
+    {
+        void OnEvent(EventType eventType);
+    }
+
+    public static class EventManager
+    {
+        static Dictionary<Type, List<IEventListenerBase>> _listeners;
+
+        public static void AddListener<EventType>(IEventListener<EventType> listener) where EventType : struct
+        {
+            Type type = typeof(EventType);
+
+            if (!_listeners.ContainsKey(type))
+            {
+                _listeners[type] = new List<IEventListenerBase>();
+            }
+
+            if (!ListenerExists(type, listener))
+            {
+                _listeners[type].Add(listener);
+            }
+        }
+
+        public static void RemoveListener<EventType>(IEventListener<EventType> listener) where EventType : struct
+        {
+            Type type = typeof(EventType);
+
+            if (!_listeners.ContainsKey(type)) return;
+
+            for (int i = 0; i < _listeners[type].Count; i++)
+            {
+                if (_listeners[type][i] == listener)
+                {
+                    _listeners[type].Remove(listener);
+
+                    if (_listeners[type].Count == 0)
+                    {
+                        _listeners.Remove(type);
+                    }
+
+                    return;
+                }
+            }
+        }
+
+        private static bool ListenerExists(Type type, IEventListenerBase listener)
+        {
+            List<IEventListenerBase> result;
+
+            if (!_listeners.TryGetValue(type, out result))
+                return false;
+
+            bool exists = false;
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (result[i] == listener)
+                {
+                    exists = true;
+                    break;
+                }
+            }
+
+            return exists;
+        }
+    }
+
+    // Helpers for easy listener registration
+    public static class EventRegister
+    {
+        public static void EventStartListening<EventType>(this IEventListener<EventType> caller) where EventType : struct
+        {
+            EventManager.AddListener<EventType>(caller);
+        }
+
+        public static void EventStopListening<EventType>(this IEventListener<EventType> caller) where EventType : struct
+        {
+            EventManager.RemoveListener<EventType>(caller);
+        }
+    }
+}
