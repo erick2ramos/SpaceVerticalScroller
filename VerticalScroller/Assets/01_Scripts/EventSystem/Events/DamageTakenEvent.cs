f923c6dd9b2cf5700805f6c954b8ecb9510f3ae3
Erick Dos Ramos <erick2ramos@gmail.com>
2020-03-05 00:15:35 -0300

    Adding damage taken event

---
+++ VerticalScroller/Assets/01_Scripts/EventSystem/Events/DamageTakenEvent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BaseSystems.EventSystem
+{
+    public struct DamageTakenEvent
+    {
+        public int DamageAmount;
+        public int PreviousHealth;
+        public int CurrentHealth;
+
+        public DamageTakenEvent(int damageAmount, int previousHealth, int currentHealth)
+        {
+            DamageAmount = damageAmount;
+            PreviousHealth = previousHealth;
+            CurrentHealth = currentHealth;
+        }
+
+        static DamageTakenEvent e;
+
+        public static void Trigger(int damageAmount, int previousHealth, int currentHealth)
+        {
+            e.DamageAmount = damageAmount;
+            e.PreviousHealth = previousHealth;
+            e.CurrentHealth = currentHealth;
+            EventManager.Trigger(e);
+        }
+    }
+}
