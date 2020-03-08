e3e6fd69e36693059824c7f92ea4c33c2d159cb8
Erick Dos Ramos <erick2ramos@gmail.com>
2020-03-07 22:26:01 -0300

    Implementing audio manager

---
+++ VerticalScroller/Assets/01_Scripts/AudioSystem/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using BaseSystems.Managers;
+using BaseSystems.EventSystem;
+using UnityEngine.Audio;
+
+namespace BaseSystems.Audio
+{
+    public class AudioManager : Manager, IEventListener<AudioSFXEvent>
+    {
+        [SerializeField]
+        AudioSource Source;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        private void OnEnable()
+        {
+            this.EventStartListening<AudioSFXEvent>();
+        }
+
+        private void OnDisable()
+        {
+            this.EventStopListening<AudioSFXEvent>();
+        }
+
+        public void OnEvent(AudioSFXEvent eventType)
+        {
+            Source.clip = eventType.Clip;
+            Source.outputAudioMixerGroup = eventType.MixerGroup;
+            Source.PlayOneShot(Source.clip);
+        }
+    }
+}
---
+++ VerticalScroller/Assets/01_Scripts/AudioSystem/AudioSFXEvent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+namespace BaseSystems.EventSystem
+{
+    public struct AudioSFXEvent
+    {
+        public AudioClip Clip;
+        public AudioMixerGroup MixerGroup;
+
+        static AudioSFXEvent e;
+
+        public static void Trigger(AudioClip clip, AudioMixerGroup group)
+        {
+            e.Clip = clip;
+            e.MixerGroup = group;
+
+            EventManager.Trigger(e);
+        }
+    }
+}
