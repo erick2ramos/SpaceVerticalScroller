7110083d32c36a4f1d05f2b2f05a0e631c8755f2
Erick Dos Ramos <erick2ramos@gmail.com>
2020-03-07 21:23:55 -0300

    Dynamic gamplay boundaries for the player

--- VerticalScroller/Assets/01_Scripts/Gameplay/Actors/ActorAbilities/CharacterMovement.cs
+++ VerticalScroller/Assets/01_Scripts/Gameplay/Actors/ActorAbilities/CharacterMovement.cs
@@ -6,13 +6,40 @@ namespace GameplayLogic
     public class CharacterMovement : CharacterAbility
     {
         public float MaxSpeed = 2;
+        public Rect GameplayArea;
         protected Vector3 Velocity;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            BoxCollider2D b2d = GetComponent<BoxCollider2D>();
+
+            if(Camera.main != null)
+            {
+                float height = Camera.main.orthographicSize;
+                float width = height * Camera.main.aspect;
+
+                GameplayArea.x = -width + (b2d.size.x * 0.5f);
+                GameplayArea.y = -height + (b2d.size.y * 0.5f);
+
+                GameplayArea.width = (width - (b2d.size.x * 0.5f)) * 2;
+                GameplayArea.height = (height - (b2d.size.y * 0.5f)) * 2;
+                
+            }
+        }
 
         protected override void ProcessInput()
         {
             Velocity = _input.MovementDirection * MaxSpeed * Time.deltaTime;
 
+            if (!GameplayArea.Contains(transform.position + Velocity))
+            {
+                return;
+            }
+
             transform.Translate(Velocity);
+
         }
     }
 }
